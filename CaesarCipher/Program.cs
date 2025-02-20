cff9b4a49f26f99e31ae4cf08fa5f231f43194af
Timberfang <timberfang.code@pm.me>
2025-02-20 15:01:52 -0600

    chore: Add copyright info

--- CaesarCipher/Program.cs
+++ CaesarCipher/Program.cs
@@ -8,6 +8,7 @@ internal static class Program
 	{
 		var inputOption = new Option<string>("--input", "Input text") { IsRequired = true };
 		var shiftOption = new Option<int>("--shift", "How much to shift the text") { IsRequired = true };
+		var aboutOption = new Option<bool>("--about", "Display copyright information about the program");
 		inputOption.AddAlias("-i");
 		shiftOption.AddAlias("-s");
 		shiftOption.AddValidator(result =>
@@ -24,6 +25,7 @@ internal static class Program
 				result.ErrorMessage = $"Shift must be between 0 and {int.MaxValue}.";
 			}
 		});
+		// aboutOption.SetDefaultValue(true);
 
 		var rootCommand = new RootCommand("Caesar cipher implementation using C#.");
 		var encryptCommand = new Command("encrypt", "Encrypt the text")
@@ -43,6 +45,7 @@ internal static class Program
 		rootCommand.AddCommand(encryptCommand);
 		rootCommand.AddCommand(decryptCommand);
 		rootCommand.AddCommand(listCommand);
+		rootCommand.AddOption(aboutOption);
 
 		encryptCommand.SetHandler((input, shift) =>
 			{
@@ -60,6 +63,67 @@ internal static class Program
 				foreach (var match in output) { Console.WriteLine($"- {match.Item2} ({match.Item1})"); }
 			},
 			inputOption);
+		rootCommand.SetHandler(displayAbout =>
+			{
+				if (!displayAbout) return;
+				// UTF-8 is needed to display the copyright symbol
+				Console.OutputEncoding = System.Text.Encoding.UTF8;
+				Console.WriteLine(string.Join(
+					Environment.NewLine,
+					"This software is made available to you under the following licenses:",
+					"",
+					"CaesarCipher",
+					"",
+					"Copyright \u00a9 2025 Timberfang",
+					"",
+					"Permission is hereby granted, free of charge, to any person obtaining a copy",
+					"of this software and associated documentation files (the \"Software\"), to deal",
+					"in the Software without restriction, including without limitation the rights",
+					"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell",
+					"copies of the Software, and to permit persons to whom the Software is",
+					"furnished to do so, subject to the following conditions:",
+					"",
+					"The above copyright notice and this permission notice shall be included in all",
+					"copies or substantial portions of the Software.",
+					"",
+					"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR",
+					"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,",
+					"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE",
+					"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER",
+					"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,",
+					"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE",
+					"SOFTWARE.",
+					"",
+					"--------------------------------------------------------------------------------",
+					"",
+					"System.CommandLine",
+					"",
+					"The MIT License (MIT)",
+					"",
+					"Copyright \u00a9 .NET Foundation and Contributors",
+					"",
+					"All rights reserved.",
+					"",
+					"Permission is hereby granted, free of charge, to any person obtaining a copy",
+					"of this software and associated documentation files (the \"Software\"), to deal",
+					"in the Software without restriction, including without limitation the rights",
+					"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell",
+					"copies of the Software, and to permit persons to whom the Software is",
+					"furnished to do so, subject to the following conditions:",
+					"",
+					"The above copyright notice and this permission notice shall be included in all",
+					"copies or substantial portions of the Software.",
+					"",
+					"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR",
+					"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,",
+					"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE",
+					"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER",
+					"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,",
+					"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE",
+					"SOFTWARE."));
+				Console.OutputEncoding = System.Text.Encoding.Default;
+			},
+			aboutOption);
 		return await rootCommand.InvokeAsync(args);
 	}
 }
